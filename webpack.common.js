ca646c2c34eb943d157e71a996768f34a02e0ade
sudo-shihab <contact.shihab.me@gmail.com>
2019-12-21 01:03:13 +0530

    added webpack-clean plugin with html and file loader for extention support

--- webpack.common.js
+++ webpack.common.js
@@ -20,6 +20,25 @@ module.exports = {
 					// Compiles Sass to CSS
 					'sass-loader'
 				]
+			},
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: 'html-loader'
+				}
+			},
+			{
+				test: /\.(svg|png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							esModule: false,
+							name: '[name].[hash].[ext]',
+							outputPath: 'images'
+						}
+					}
+				]
 			}
 		]
 	},
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,13 @@
 const webpackCommon = require('./webpack.common.js');
 const path = require('path');
 const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(webpackCommon, {
 	mode: 'production',
 	output: {
 		filename: 'app.bundle.[contentHash].js',
 		path: path.resolve(__dirname, 'dist')
-	}
+	},
+	plugins: [ new CleanWebpackPlugin() ]
 });
