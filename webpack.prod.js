6f9a27996258bdc82fcc20a747d3cc933b7b4b76
sudo-shihab <contact.shihab.me@gmail.com>
2019-12-26 01:50:03 +0530

    some configs changed

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
 module.exports = merge(webpackCommon, {
 	devtool: 'source-map',
@@ -12,6 +14,21 @@ module.exports = merge(webpackCommon, {
 	output: {
 		filename: '[name][contentHash].min.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+			}
+		]
+	},
+	// chunks
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
 	},
 	plugins: [
 		new MiniCssExtractPlugin({ filename: '[name][contentHash].min.css' }),
@@ -24,26 +41,19 @@ module.exports = merge(webpackCommon, {
 				removeComments: true,
 				collapseWhitespace: true
 			}
+		}),
+		new CompressionPlugin({
+			filename: '[path].gz[query]',
+			algorithm: 'gzip',
+			test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$|\.jpg?.$/,
+			threshold: 8192, // keep it 0 to test : make all file compresed
+			minRatio: 0.7 // keep it 1 to test :  make all file compressed
+		}),
+		new BrotliPlugin({
+			asset: '[path].br[query]',
+			test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$|\.jpg?.$/,
+			threshold: 8192, // keep it 0 to test : make all file compresed
+			minRatio: 0.7 // keep it 1 to test :  make all file compressed
 		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-			}
-		]
-	},
-	// chunks
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					test: /[\\/]node_modules[\\/]/, // this is what you are looking for
-					name: 'vendor',
-					chunks: 'all'
-				}
-			}
-		}
-	}
+	]
 });
